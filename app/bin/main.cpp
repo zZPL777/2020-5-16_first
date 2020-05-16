c0224996e4a518c699a6155c28c60548be5cf798
pengliz <1033557912@qq.com>
2020-05-16 21:15:12 +0800

    撤回操作

--- app/bin/main.cpp
+++ app/bin/main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 
 using namespace std;
+int com(int a,int b)
+{
+    return (a>b)? a:b;
+}
 int main()
 {
     cout<<"this is my first file";
+    int a=1,b=2;
+    cout<<com(a,b)<<endl;
     return 0;
 }
